42eff1daa06aa75ba788e12256ae43da054f1852
neda <neda.ghodrati@gmail.com>
2021-11-07 09:02:38 -0800

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This Project is created by Neda Ghodrati, and it is{" "}
-        <a
-          href="https://github.com/neda-ghodrati/my-react-weather-app"
-          target="-blank"
-        >
-          {" "}
-          Open-Sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <botton className="btn btn-primary"> Hello</botton>
+        <footer>
+          This Project is created by Neda Ghodrati, and it is{" "}
+          <a
+            href="https://github.com/neda-ghodrati/my-react-weather-app"
+            target="-blank"
+          >
+            {" "}
+            Open-Sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
