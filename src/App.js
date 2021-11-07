1c2715ede715c4759f7f6f144abe6b1aa8a39155
neda <neda.ghodrati@gmail.com>
2021-11-07 08:31:08 -0800

    Unnecessary files deleted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      Weather App
+      <footer>
+        This Project is created by Neda Ghodrati, and it is{" "}
+        <a href="" target="-blank">
+          {" "}
+          Open-Sourced on GitHub
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
