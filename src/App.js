e51b653b386b3ff94bfe9b73714c505b0bcb5931
neda <neda.ghodrati@gmail.com>
2021-11-07 08:39:47 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
       Weather App
       <footer>
         This Project is created by Neda Ghodrati, and it is{" "}
-        <a href="" target="-blank">
+        <a
+          href="https://github.com/neda-ghodrati/my-react-weather-app"
+          target="-blank"
+        >
           {" "}
           Open-Sourced on GitHub
         </a>
